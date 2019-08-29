ebc06062f8c2b8b1df2cccd97920c74eee6399f5
Fernando720 <fhac@cin.ufpe.br>
2019-08-29 14:58:58 -0300

    comecei o joguinho

--- tryout.py
+++ tryout.py
@@ -1,2 +1,14 @@
 
-print ("oi")
+noJogo = True
+posicaoX = 8
+posicaoY = 3
+
+while(noJogo):
+    print("você está lascado para sair desta casa. "+
+          "Precisa encontrar uma chave escondida no escuro. "+
+          "Existem móveis próximos a você, cuidado ao esbarrar. "+
+          "No mais, é isso. Te vira, boy!!")
+    comando = input("que ação desejas tomar? ")
+          if (comando == "sentar")
+              
+              
